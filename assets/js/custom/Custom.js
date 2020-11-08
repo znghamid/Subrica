b073fb2a9c7bf46790f39a6d464aae8670b14a38
Hamidreza Zangooie <hrzangoie@gmail.com>
2020-11-08 19:03:19 +0330

    add more item to site

--- assets/js/custom/Custom.js
+++ assets/js/custom/Custom.js
@@ -1,21 +1,26 @@
 $(document).ready(function(){
+
     $('#menu-icon').click(function(){
         $('body').toggleClass('hide-overflow');
         $(this).toggleClass('open');
         $('.menu').toggleClass('menu-open');
     });
+    
     $('#search-icon').click(function(){
         $('#SearchPage').addClass('search-page-open');
     });
+    
     $('#CloseSearhPage').click(function(){
         $('#SearchPage').removeClass('search-page-open');
     });
+    
     $(window).on('load',function(){
         $(".menu").mCustomScrollbar({
             axis:"y",
             theme:"dark",
         });
     });
+    
     jQuery(function(){
         var minimized_elements = $('p.minimize');
         var minimize_character_count = 110;    
@@ -29,4 +34,19 @@ $(document).ready(function(){
             );
         }); 
     });
+
+    var BackToTopButton = $('#BackToTopButton');
+    $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        BackToTopButton.addClass('show');
+    } else {
+        BackToTopButton.removeClass('show');
+    }
+    });
+
+    BackToTopButton.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+    });
+
 });
